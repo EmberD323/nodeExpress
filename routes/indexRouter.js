e2da7b157070f84efd680c8d949a5be6c5d86a2c
EmberD323 <doyle.ember@gmail.com>
2024-12-03 09:25:54 -0700

    Remove logout

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,10 +4,5 @@ const userController = require("../controllers/userController");
 
 indexRouter.post("/signup", userController.newUserCreate);
 indexRouter.post('/login', userController.logIn);
-//logout
-indexRouter.get("/logout", (req, res, next) => {
-    //finish on front end
-    res.json("logged out")
-});
 
 module.exports = indexRouter;
